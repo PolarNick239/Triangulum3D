f4c2cc847e1a16fe21b003d57484a51a172edb3d
Nikolay Polyarniy <PolarNick239@gmail.com>
2016-03-03 12:03:37 +0300

    reconstruct_test: decoupled to scene creation and testing

--- tests/triangulum_test/scanner/reconstruct_test.py
+++ tests/triangulum_test/scanner/reconstruct_test.py
@@ -31,16 +31,19 @@ class ReconstructionBuilderTest(TestBase):
         color, depth = asyncio.get_event_loop().run_until_complete(render())
         return color, depth
 
-    def reconstruction_test(self):
+    def box_test(self):
         scene = Scene()
         scene.add_renderable(Box([-1, 1], [-0.5, 0.5], [0, 1]))
         camera = Camera(course=40)
         viewport = (500, 300)
         projector_lods = 8
+        
+        self.reconstruction_case('box', scene, camera, viewport, projector_lods)
 
+    def reconstruction_case(self, scene_name, scene, camera, viewport, projector_lods):
         color, depth = self.render(scene, camera, viewport)
-        self.dump_debug_img("scene_color.png", color)
-        self.dump_debug_img("scene_depth.png", support.array_to_grayscale(depth))
+        self.dump_debug_img(Path(scene_name) / "scene_color.png", color)
+        self.dump_debug_img(Path(scene_name) / "scene_depth.png", support.array_to_grayscale(depth))
 
         central_line_processor = CentralLineExtractionProcessor(debug_enabled=self.with_debug_output())
         reconstructor = ReconstructionBuilder(line_extraction_processor=central_line_processor)
@@ -58,10 +61,10 @@ class ReconstructionBuilderTest(TestBase):
         points_3d = reconstructor.build_point_cloud(projector, camera)
 
         if self.with_debug_output():
-            subdir = Path('central_line')
+            subdir = Path(scene_name) / 'central_line'
             self.dump_debug_matrix_by_hue(subdir / 'is_edge_pixel.png', central_line_processor._debug_last_values['is_edge_pixel'])
             self.dump_debug_matrix_by_hue(subdir / 'distance.png', central_line_processor._debug_last_values['distance'])
             self.dump_debug_matrix_by_hue(subdir / 'is_maximum.png', central_line_processor._debug_last_values['is_maximum'])
 
-            ply.write_ply(self.debug_dir() / 'points.ply', points_3d)
+            ply.write_ply(self.debug_dir() / scene_name / 'points.ply', points_3d)
         # TODO: implement result check
